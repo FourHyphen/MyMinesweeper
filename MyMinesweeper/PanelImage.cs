04c32a9a22df61d759a14be587bcf18e0bcae3de
FourHyphen <FourHyphen.4@gmail.com>
2021-05-30 09:49:14 +0900

    Panelの画像を扱う処理を別クラスにカプセル化

---
+++ MyMinesweeper/PanelImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MyMinesweeper
+{
+    public class PanelImage
+    {
+        public BitmapSource ImageClosing { get; private set; }
+
+        public BitmapSource ImageOpenedMine { get; private set; }
+
+        public BitmapSource ImageOpened { get; private set; }
+
+        public PanelImage(int panelSize)
+        {
+            ImageClosing = ImageProcess.GetShowImage("./Resource/Image/Closing.png", panelSize, panelSize);
+            ImageOpenedMine = ImageProcess.GetShowImage("./Resource/Image/OpenedMine.png", panelSize, panelSize);
+            ImageOpened = ImageProcess.GetShowImage("./Resource/Image/Opened.png", panelSize, panelSize);
+        }
+    }
+}
---
+++ TestMyMinesweeper/TestPanelImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMinesweeper;
+
+namespace TestMyMinesweeper
+{
+    [TestClass]
+    public class TestPanelImage
+    {
+        private string BeforeEnvironment { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        [TestMethod]
+        public void TestInitialize()
+        {
+            PanelImage pi = new PanelImage(20);
+        }
+    }
+}
