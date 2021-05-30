6e4d23774073de5622073bb4b90e2628eb4368f7
FourHyphen <FourHyphen.4@gmail.com>
2021-05-30 09:21:38 +0900

    地雷を踏んだらゲームオーバーになる単体テストを追加

--- TestMyMinesweeper/TestPanels.cs
+++ TestMyMinesweeper/TestPanels.cs
@@ -15,5 +15,23 @@ namespace TestMyMinesweeper
             Assert.AreEqual(expected: 2, actual: panels.GetNumMine());
             Assert.AreEqual(expected: 0, actual: panels.GetNumOpened());
         }
+
+        [TestMethod]
+        public void TestGameOverWhenPanelOpenedIsMine()
+        {
+            Panels panels = new Panels("Debug");
+            Assert.IsFalse(panels.IsGameOver());
+
+            // 非地雷パネルのオープンチェック
+            Assert.AreEqual(expected: Panel.PanelStatus.Closing, actual: panels.GetStatus(0, 0));
+            panels.Open(0, 0);
+            Assert.AreEqual(expected: Panel.PanelStatus.Opened, actual: panels.GetStatus(0, 0));
+            Assert.IsFalse(panels.IsGameOver());
+
+            // 地雷パネルのオープンチェック
+            panels.Open(0, 4);
+            Assert.AreEqual(expected: Panel.PanelStatus.Opened, actual: panels.GetStatus(0, 4));
+            Assert.IsTrue(panels.IsGameOver());
+        }
     }
 }
